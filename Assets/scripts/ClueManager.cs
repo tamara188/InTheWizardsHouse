71516dea21b826eba7ba6c6269bce52c60465853
tamara188 <tamaragruenewald@gmail.com>
2022-10-01 03:49:29 -0600

    adding a clue

---
+++ Assets/scripts/ClueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueManager : MonoBehaviour
+{
+    public GameObject metronome;
+    private float timer = 0f;
+    GameObject[] allClues = GameObject.FindGameObjectsWithTag("Clue");
+    public SpriteRenderer clueSprite;
+    public Sprite active;
+    public Sprite inactive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    float currentTime = 0f;
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if(timer % 10 == 0)
+        {
+            currentTime = timer;
+            int randy = (int)Random.Range(0f, allClues.Length+1);
+            clueSprite.sprite = active;
+        }
+        if(timer == currentTime + 2)
+        {
+            clueSprite.sprite = inactive;
+        }
+
+    }
+}
