5a8e86527f2c53a99399276a1e307acf7df210d2
Tamara Gruenewald <tamaragruenewald@gmail.com>
2022-10-01 22:14:37 -0600

    title scene now transitions to game

---
+++ Assets/scripts/sceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class sceneManager : MonoBehaviour
+{
+     int n;
+   public void OnButtonPress(){
+      n++;
+      Debug.Log("Button clicked " + n + " times.");
+      SceneManager.LoadScene (sceneName:"playArea");
+   }
+}
