8072a64c8114b17bc4805f3116a1dd283bf0f1f6
Tamara Gruenewald <tamaragruenewald@gmail.com>
2022-10-02 15:20:42 -0600

    last minite panic

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class movement : MonoBehaviour
+
 {
     public GameObject cam;
     public GameObject player;
     public int speed;
     public int leftBound;
     public int rightBound;
+    public Animator anim;
     // Update is called once per frame
+    void processAnimation(){
+        anim.SetFloat("horizontal",500);
+    }
     void Update()
     {
+        processAnimation();
         if (Input.GetKey(KeyCode.A))
         {
             if(transform.position[0] > leftBound)
             {
                 transform.position += Vector3.left * speed * Time.deltaTime;
                 cam.transform.position += Vector3.left * speed * Time.deltaTime;
+                anim.SetFloat("horizontal",0);
             }
             
         }
@@ -27,8 +33,10 @@ public class movement : MonoBehaviour
             {
                 transform.position += Vector3.right * speed * Time.deltaTime;
                 cam.transform.position += Vector3.right * speed * Time.deltaTime;
+                anim.SetFloat("horizontal",1);
             }
 
         }
+        anim.SetFloat("horizontal",0.5f);
     }
 }
